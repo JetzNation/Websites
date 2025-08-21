870cedb52cf2b1187907a15e847d58716b44033f
JetzNation <andyskabelund2@gmail.com>
2025-08-21 11:10:47 -0600

    Delete Worth your weight in gold directory

--- Worth your weight in gold/gold/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'gold'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('plan', views.plan, name='plan'),
-]
--- Worth your weight in gold/gold/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    return render(request, 'gold/index.html')
-
-def plan(request):
-    return render(request, 'gold/plan.html')
